f259c0e0fc2f4acf23314abf902a96dc24f927e7
hasun999 <33555073+hasun999@users.noreply.github.com>
2022-12-08 21:49:00 +0530

    Add initial E-commerce Website

---
+++ scripts.js
@@ -0,0 +1,35 @@
+// for Shoes //
+function newImage1() {
+    document.getElementById('img1').src="Thumbnail/shoes2.webp";
+}
+
+function oldImage1() {
+    document.getElementById('img1').src="Thumbnail/shoes.webp";
+}
+
+// for Cloths //
+function newImage2() {
+    document.getElementById('img2').src="Thumbnail/clothing2.webp";
+}
+
+function oldImage2() {
+    document.getElementById('img2').src="Thumbnail/clothing.webp";
+}
+
+// for BestSeller //
+function newImage3() {
+    document.getElementById('img3').src="Thumbnail/best-seller2.jpg";
+}
+
+function oldImage3() {
+    document.getElementById('img3').src="Thumbnail/best seller.webp";
+}
+
+// New Arrival //
+function newImage4() {
+    document.getElementById('img4').src="Thumbnail/new-arrival2.webp";
+}
+
+function oldImage4() {
+    document.getElementById('img4').src="Thumbnail/new-arrival.webp";
+}
